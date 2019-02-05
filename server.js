eb35a7fefe377c721207e6162e497e142f887e7a
Tim Vlasuk <timvlasuk@gmail.com>
2019-02-05 11:38:05 -0800

    updated project-planning/brainstorming notepad

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const express = require('express');
-
+const bodyParser = require('bodyParser')
 
 const app = express()
+
+app.use(express.static('/views'))
+
+app.route('/')
+    .get(function(req, res) {
+
+    })
+
+    .post(function(req, res) {
+
+    })
+
+    .put(function(req, res) {
+
+    })
+
+    .delete(function(req, res) {
+        
+    })
+
+
+
+/*allot space for testing runServer/closeServer*/
+
+app.all('*', function() {
+    /* catch-all - http://expressjs.com/en/4x/api.html#app.all */
+    res.status(501)
+})
