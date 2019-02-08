9937e09183e7d77c99aeb0466bc6070a0cb357ca
Tim Vlasuk <timvlasuk@gmail.com>
2019-02-08 09:19:32 -0800

    added dotenv and passport-local stategy - working on implementing w/ jwt

--- server.js
+++ server.js
@@ -1,21 +1,59 @@
+
+require('dotenv').config(); /*https://www.npmjs.com/package/dotenv */
 const express = require('express');
 const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+const { User, Post, Comments } = require('./models')
+
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+
+// passport.use(new LocalStrategy(
+// 	function(usr, pwd, done) {
+// 		User.findOne({userName: usr}, function(err, usr) {
+// 			if(err) {return done(err) }
+// 			if (!usr) {
+// 				return done(null, false, {message: 'Invalid username!'})
+// 			}
+// 			if (!user.validPassword(pwd)) {
+// 				return done(null, false, {message: 'Invalid Password!!'})
+// 			}
+// 			return done(null, usr);
+// 		});
+// 	}
+// ));
 
 const app = express()
 
-app.use(express.static('/views'))
+app.use(express.static('./views'))
 
-app.get('/Home', function(req, res) {
+app.get('/', function(req, res) {
         console.log("obtained GET route")
+        res.status(200).end()
 
     });
-    
+
+app.post(jsonParser, function(req, res) {
+
+console.log('authenticatin', req.body)
+res.status(200).end()
+})
+
+
+
+
+
+
 
 
 
 /*allot space for testing runServer/closeServer*/
 
-app.all('*', function() {
-    /* catch-all - http://expressjs.com/en/4x/api.html#app.all */
-    res.status(501)
-})
+ app.all('/*', function() {
+     /* catch-all - http://expressjs.com/en/4x/api.html#app.all */
+     console.log('no such location!')
+     res.status(501)
+ })
+
+app.listen(8080)
