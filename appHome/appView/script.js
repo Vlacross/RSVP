4f6e5acbcb9e733ebd058feedf7c479ad9b37aef
Tim Vlasuk <timvlasuk@gmail.com>
2019-02-09 17:06:41 -0800

    added new sub-dir for appHome

---
+++ appHome/appView/script.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function promptAuth() {
+    const loginForm = `
+    <form action="../loggin" class="othForm">
+			<label for="userNameInput">UserId
+				<input id="userNameInput" name="userNameInput" class="userNameInput" type="text">
+			</label>
+			<label for="userPassInput">PassWord
+				<input id="userPassInput" class="userPassInput" type="text">
+			</label>
+		</form>
+		<input class="submit" type="submit" val="submit">`
+
+		$('.loadLogin').append(loginForm)
+
+	
+		$('body').on('click', '.submit', function(e) {
+			e.preventDefault();
+			const id = document.getElementsByClassName('userNameInput')[0];
+			const pwd = document.getElementsByClassName('userPassInput')[0];
+			const logIn = {
+					userName: id.value,
+					passWord: pwd.value
+				};
+			fetch('../loggin', {
+				method: 'post',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(logIn)
+			})
+			.then(res => console.log(res.status))
+			.catch(err => console.log(err))
+		})
+};
+
+
+$(document).ready(function() {
+    console.log('hittingJQscript!')
+		promptAuth()
+		
+})
---
+++ appHome/router.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express.Router()
+
+const mongoose = require('mongoose')
+
+const {}
+
+
+app.get('/', function(req, res) {
+
+});
+
+app.get('/:id', function(req, res) {
+
+});
+
+app.post('/', function(req, res) {
+
+});
+app.put('/:id', function(req, res) {
+
+});
+
+app.delete('/:id', function(req, res) {
+
+});
+
+module.exports = app
