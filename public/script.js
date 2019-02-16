40b737cd577a2f151c0be517d506ba60ab82150f
Tim Vlasuk <timvlasuk@gmail.com>
2019-02-15 17:55:14 -0800

    delete template

--- public/script.js
+++ public/script.js
@@ -15,29 +15,16 @@
 
 
 function promptAuth() {
-    const loginForm = `
-    <form action="../loggin" class="othForm">
-			<label for="userNameInput">UserId
-				<input id="userNameInput" name="userNameInput" class="userNameInput" type="text">
-			</label>
-			<label for="userPassInput">PassWord
-				<input id="userPassInput" class="userPassInput" type="text">
-			</label>
-		</form>
-		<input class="submit" type="submit" val="submit">`
-
-		$('.loadLogin').append(loginForm)
-
-	
-		$('body').on('click', '.submit', function(e) {
+		$('body').on('submit', '.othForm', function(e) {
 			e.preventDefault();
 			const id = document.getElementsByClassName('userNameInput')[0];
 			const pwd = document.getElementsByClassName('userPassInput')[0];
 			const logIn = {
-					userName: id.value,
-					passWord: pwd.value
+					username: id.value,
+					password: pwd.value
 				};
-			fetch('../loggin', {
+				
+			fetch('login', {
 				method: 'post',
 				headers: {
 					'Accept': 'application/json',
